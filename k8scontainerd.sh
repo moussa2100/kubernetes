f25ef650d8ecc741f0ba86ac6e3579424b0f4cc7
Mohamed Moussa <37773718+moussa2100@users.noreply.github.com>
2023-11-13 00:37:40 +0300

    Update k8scontainerd.sh

--- k8scontainerd.sh
+++ k8scontainerd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt install -y wget curl nano git
+
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
@@ -8,22 +10,25 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-# sysctl params required by setup, params persist across reboots
+
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
+
 sudo sysctl --system
 
 wget https://github.com/containerd/containerd/releases/download/v1.6.24/containerd-1.6.24-linux-amd64.tar.gz
+
 tar Cxzvf /usr/local containerd-1.6.24-linux-amd64.tar.gz
 
 wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
 
 sudo mkdir -p /usr/local/lib/systemd/system/
 sudo cp containerd.service /usr/local/lib/systemd/system/
+
 systemctl enable containerd
 systemctl start containerd
 
@@ -34,10 +39,12 @@ wget https://github.com/containernetworking/plugins/releases/download/v1.2.0/cni
 mkdir -p /opt/cni/bin
 tar Cxzvf /opt/cni/bin /tmp/cni-plugins-linux-amd64-v1.2.0.tgz
 
-mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
 
-swapoff -a
+mkdir -p /etc/containerd
+sudo wget https://raw.githubusercontent.com/moussa2100/kubernetes/main/config.toml -O /etc/containerd/config.toml
+
+
+systemctl restart containerd
 
 sudo apt-get update
 
@@ -45,14 +52,16 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 
 sudo mkdir -p /etc/apt/keyrings/
 
+
 curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 
-sudo apt-get install -y kubelet kubeadm kubectl
 
+sudo apt-get install -y kubelet kubeadm kubectl
+	
 sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo swapoff -a
